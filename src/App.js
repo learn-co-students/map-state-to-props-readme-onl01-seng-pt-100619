4b1caa112c34d3d8717879bf5fcec95ee509b7e8
allisoncortez <allizetorc@gmail.com>
2020-07-23 16:53:58 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'; /* code change */
 import './App.css';
 
 class App extends Component {
@@ -21,4 +22,19 @@ class App extends Component {
   }
 };
 
-export default App;
+// filters out the changes relevant to a particular component:
+// in mapStateToProps() we specify exactly which slice of the state we want to provide to our component
+const mapStateToProps = (state) => {
+  return { items: state.items };
+};
+
+// CONNECT: a function that listens to every change in the store
+// it is synced up to our store
+// When a change occurs, it calls a function that we write called mapStateToProps()
+export default connect(mapStateToProps)(App);
+
+
+// we have to say which component in our app we are providing this data to:
+// connect(mapStateToProps)(App)
+
+//this entire connect() method returns a new App component (with the correct data)
